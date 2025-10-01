7a41000515964e5469cea5f46f7d7be40981b0ff
PedroOBertoni <pedroolivbertoni@gmail.com>
2025-10-01 10:04:40 -0300

    FEAT: Setting Up Slider Screens in MainActivity and Adding an Icon

--- app/src/main/java/com/aula/organizze/MainActivity.java
+++ app/src/main/java/com/aula/organizze/MainActivity.java
@@ -9,7 +9,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 
-import com.aula.firebaseapp.R;
+import com.aula.organizze.R;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
 public class MainActivity extends IntroActivity {
 
@@ -17,13 +18,34 @@ public class MainActivity extends IntroActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide( new com.heinrichreimersoftware.materialintro.slide.FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .build());
+
+        addSlide( new com.heinrichreimersoftware.materialintro.slide.FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build());
+
+        addSlide( new com.heinrichreimersoftware.materialintro.slide.FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_3)
+                .build());
+
+        addSlide( new com.heinrichreimersoftware.materialintro.slide.FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_4)
+                .build());
     }
 }
