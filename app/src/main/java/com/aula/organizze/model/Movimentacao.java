ccf6eecda736fc6a4caa3152a9a4c48baa2b309e
PedroOBertoni <pedroolivbertoni@gmail.com>
2025-10-09 10:55:35 -0300

    FEAT: Creating and configuring the Movimentacao class

---
+++ app/src/main/java/com/aula/organizze/model/Movimentacao.java
@@ -0,0 +1,44 @@
+package com.aula.organizze.model;
+
+public class Movimentacao {
+
+    private double valor;
+    private String titulo;
+    private String descricao;
+    private String data;
+
+    public Movimentacao() {
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
